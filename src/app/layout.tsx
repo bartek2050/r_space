99307df58a029ec40a051d18d074a2eedce3a222
bartek2050 <bartlomiej.ciastek@gmail.com>
2025-03-17 22:19:49 +0100

    update globals.css and create home view

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,25 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import type {Metadata} from "next";
+import {Geist_Mono} from "next/font/google";
 import "./globals.css";
+import {ReactNode} from "react";
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
+    weight: "300"
 });
 
 export const metadata: Metadata = {
-  title: "RefSpace",
-  description: "Recruitment task",
+    title: "RefSpace",
+    description: "Recruitment task",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body
-        className={` ${geistMono.variable} antialiased`}
-      >
+export default function RootLayout({children}: { children: ReactNode }) {
+    return (
+        <html lang="pl">
+        <body className={` ${geistMono.variable} antialiased`}>
         {children}
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
